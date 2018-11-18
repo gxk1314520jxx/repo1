8e874a96e9c8c2efd668ecfbdf966313821d287b
admin <admin@itcast.cn>
2018-11-18 23:17:08 +0800

    再次修改

--- lucene3/src/com/theima/lucene/Index.java
+++ lucene3/src/com/theima/lucene/Index.java
@@ -40,6 +40,7 @@ public class Index {
            
             System.out.println("结束" + "-------------------------------");
 			  System.out.println("结束" + "我爱靳雪霞");
+			  System.out.println("结束" + "我爱靳雪霞");
         }
     }
 
