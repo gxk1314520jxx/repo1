048d0e38798e1ad845d784873d700b3201f4dbb7
gxk1314520jxx <2049307588@qq.com>
2020-04-08 02:01:04 +0800

    节点：4 提交人：关晓坤

--- lucene3/src/com/theima/lucene/Index.java
+++ lucene3/src/com/theima/lucene/Index.java
@@ -46,6 +46,7 @@ public class Index {
 			  System.out.println("结束" + "我爱靳雪霞");
              System.out.println("结束" + "我爱靳雪霞");
 			  System.out.println("结束" + "我爱靳雪霞");
+			  System.out.println("结束" + "我爱靳雪霞");
         }
     }
 
