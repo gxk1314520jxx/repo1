e8e7f47d3f53e039fed61b59c0e02751bb3a8c10
gxk1314520jxx <2049307588@qq.com>
2020-04-08 22:49:25 +0800

    节点12 提交人 2关晓坤

--- lucene3/src/com/theima/lucene/Index.java
+++ lucene3/src/com/theima/lucene/Index.java
@@ -11,15 +11,6 @@ public class Index {
 
     @Test
     public void testIKAnalyzer() throws IOException {
-        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
-        //设置偏移量,记录关键字的起始与结束位置
-        OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
-        //将指针重置在列表头部
-        tokenStream.reset();
-        //遍历tokenStream,通过incrementToken方法判断列表是否结束
-        while (tokenStream.incrementToken()) {
-            System.out.println("测试了");
-        }
     }
 
 
